1bb980df51d5de9e9a145b5a1df2f1e0c5158852
Soepaza <soefreestyle@gmail.com>
2023-06-13 22:19:40 -0300

    Novos argumentos para potencializar o uso da função

---
+++ functions_train/organizar_bebidas_2.0.py
@@ -0,0 +1,31 @@
+#trabalhando com novos parametros para potencializar o uso da função.
+produtos = ['beb46275','TFA23962','TFA64715',
+            'TFA69555','TFA56743','BSA45510',
+            'TFA44968','CAR75448','CAR23596',
+            'CAR13490','BEB21365','BEB31623',
+            'BSA62419','BEB73344','TFA20079',
+            'BEB80694','BSA11769','BEB19495',
+            'TFA14792','TFA78043','BSA33484',
+            'BEB97471','BEB62362','TFA27311',
+            'TFA17715','BEB85146','BEB48898',
+            'BEB79496','CAR38417','TFA19947',
+            'TFA58799','CAR94811','BSA59251',
+            'BEB15385','BEB24213','BEB56262',
+            'BSA96915','CAR53454','BEB75073'
+]
+
+def cod_da_categoria(bebida, cod_produto):
+    bebida = bebida.upper()
+    if cod_produto in bebida:
+        return True
+    else:
+        return False
+
+
+for produto in produtos:
+    if cod_da_categoria(produto, 'BEB'):
+        print(f' Enviar {produto} para o setor de bebidas alcóolicas.')
+    elif cod_da_categoria(bebida = produto, cod_produto = 'CAR'):
+        print(f' Enviar {produto} para o setor de bebidas importadas.')
+
+#podendo continuar percorrendo a lista usando os argumentos da função
